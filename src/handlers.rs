fc067641875c707dd78df98d30b63d795a48141f
rednithin <reddy.nithinpg@live.com>
2020-09-28 18:33:23 +0530

    Hardcoding BodyBrowse Response.

--- src/handlers.rs
+++ src/handlers.rs
@@ -48,7 +48,54 @@ pub fn content_handler() -> BoxedFilter<(impl Reply,)> {
             log::error!("{}", action);
             log::error!("\n{}", body_string);
 
-            warp::reply()
+            let response = format!(r#"<?xml version="1.0" encoding="utf-8"?>
+            <s:Envelope
+                xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+                <s:Body>
+                    <u:BrowseResponse
+                        xmlns:u="urn:schemas-upnp-org:service:ContentDirectory:1">
+                        <Result>
+                            &lt;DIDL-Lite
+                                xmlns:dc=&quot;http://purl.org/dc/elements/1.1/&quot;
+                                xmlns:upnp=&quot;urn:schemas-upnp-org:metadata-1-0/upnp/&quot;
+                                xmlns=&quot;urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/&quot;&gt;
+                                &lt;container id=&quot;1&quot; parentID=&quot;0&quot; childCount=&quot;2&quot; restricted=&quot;false&quot;&gt;
+                                    &lt;dc:title&gt;My Music&lt;/dc:title&gt;
+                                    &lt;upnp:class&gt;object.container.storageFolder&lt;/upnp:class&gt;
+                                    &lt;upnp:storageUsed&gt;730000&lt;/upnp:storageUsed&gt;
+                                    &lt;upnp:writeStatus&gt;WRITABLE&lt;/upnp:writeStatus&gt;
+                                    &lt;upnp:searchClass includeDerived=&quot;false&quot;&gt;object.container.album.musicAlbum&lt;/upnp:searchClass&gt;
+                                    &lt;upnp:searchClass includeDerived=&quot;false&quot;&gt;object.item.audioItem.musicTrack&lt;/upnp:searchClass&gt;
+                                    &lt;upnp:createClass includeDerived=&quot;false&quot;&gt;object.container.album.musicAlbum&lt;/upnp:createClass&gt;
+                                &lt;/container&gt;
+                                &lt;container id=&quot;2&quot; parentID=&quot;0&quot; childCount=&quot;2&quot; restricted=&quot;false&quot;&gt;
+                                    &lt;dc:title&gt;My Photos&lt;/dc:title&gt;
+                                    &lt;upnp:class&gt;object.container.storageFolder&lt;/upnp:class&gt;
+                                    &lt;upnp:storageUsed&gt;177000&lt;/upnp:storageUsed&gt;
+                                    &lt;upnp:writeStatus&gt;WRITABLE&lt;/upnp:writeStatus&gt;
+                                    &lt;upnp:searchClass includeDerived=&quot;false&quot;&gt;object.container.album.photoAlbum&lt;/upnp:searchClass&gt;
+                                    &lt;upnp:searchClass includeDerived=&quot;false&quot;&gt;object.item.imageItem.photo&lt;/upnp:searchClass&gt;
+                                    &lt;upnp:createClass includeDerived=&quot;false&quot;&gt;object.container.album.photoAlbum&lt;/upnp:createClass&gt;
+                                &lt;/container&gt;
+                                &lt;container id=&quot;30&quot; parentID=&quot;0&quot; childCount=&quot;2&quot; restricted=&quot;false&quot;&gt;
+                                    &lt;dc:title&gt;Album Art&lt;/dc:title&gt;
+                                    &lt;upnp:class&gt;object.container.storageFolder&lt;/upnp:class&gt;
+                                    &lt;upnp:storageUsed&gt;40000&lt;/upnp:storageUsed&gt;
+                                    &lt;upnp:writeStatus&gt;WRITABLE&lt;/upnp:writeStatus&gt;
+                                    &lt;upnp:searchClass name=&quot;Vendor Album Art&quot; includeDerived=&quot;true&quot;&gt;object.item.imageItem.photo.vendorAlbumArt&lt;/upnp:searchClass&gt;
+                                    &lt;upnp:createClass includeDerived=&quot;true&quot;&gt;object.item.imageItem.photo.vendorAlbumArt&lt;/upnp:createClass&gt;
+                                &lt;/container&gt;
+                            &lt;/DIDL-Lite&gt;
+                        </Result>
+                        <NumberReturned>2</NumberReturned>
+                        <TotalMatches>2</TotalMatches>
+                        <UpdateId>10</UpdateId>
+                    </u:BrowseResponse>
+                </s:Body>
+            </s:Envelope>"#);
+
+            response
         })
+        .with(warp::reply::with::header("Content-type", "text/xml"))
         .boxed()
 }
